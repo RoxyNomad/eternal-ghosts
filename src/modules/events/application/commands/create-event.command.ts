2b1273bc285eb0c766c0046dd63e129cc814b6f2
RoxyNomad <roxana_zwicky@roxynomad.com>
2026-09-24 12:58:38 +0200

    fix: event

--- src/modules/events/application/commands/create-event.command.ts
+++ src/modules/events/application/commands/create-event.command.ts
@@ -1,7 +1,7 @@
 export class CreateEventCommand {
     constructor(
         public readonly title: string,
-        public readonly date: string,
+        public readonly date: Date,
         public readonly location?: string
     ) {}
 }
